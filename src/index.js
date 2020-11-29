4487e5625dfb84a87da499eb71b886a08f4288f2
maryna <merry.kosiv@gmail.com>
2020-11-29 18:37:45 +0200

    add global state to application

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
-// import { GithubProvider } from './context/context';
+ import * as serviceWorker from './serviceWorker';
+ import { GithubProvider } from './context/context';
 // import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
     <React.StrictMode>
-       <App/>
+        <GithubProvider>
+            <App/>
+        </GithubProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
