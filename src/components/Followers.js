8ab2f1b5004d71d6419970a337611715f9e30a58
maryna <merry.kosiv@gmail.com>
2020-12-02 20:10:50 +0200

    add: Followers component ui

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {GithubContext} from "../context/context";
 import styled from 'styled-components';
 
 const Followers = () => {
+
+    const {followers} = React.useContext(GithubContext);
+
     return (
-        <h2>Followers Component</h2>
+        <Wrapper>
+            <div className='followers'>
+                {followers.map((follower, index) => {
+                    const {avatar_url: img, html_url, login} = follower;
+                    return (
+                        <article key={index}>
+                            <img src={img}
+                                 alt={login}/>
+                            <div>
+                                <h4>{login}</h4>
+                                <a href={html_url}>{html_url}</a>
+                            </div>
+                        </article>
+                    )
+                })}
+            </div>
+        </Wrapper>
     )
 };
 
