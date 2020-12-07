05ecf318fbf60deeca792ebabd75f5d5914a0745
maryna <merry.kosiv@gmail.com>
2020-12-07 18:08:26 +0200

    added logic to form from search component

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -6,6 +6,7 @@ import { GithubContext } from '../context/context';
 const Dashboard = () => {
     return (
         <main>
+            <Search/>
             <Info />
             <User />
             <Repos/>
